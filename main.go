26890f6369f1f5dd27d3d9269e4704c0c2fdbd2a
福田皓一 <k.fukuda@shift7.jp>
2021-01-29 17:13:06 +0900

    push

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
 import (
-	"github.com/labstack/echo"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"net/http"
 	"os"
 )
 
-type Book struct {
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-
 func main() {
-	// Echo インスタンスをNew
-	e := echo.New()
-
-	// ルーティング設定
-	e.GET("/books/:id", bookHandler)
-
-	// サーバ起動
-	e.Start(":" + os.Getenv("PORT"))
+	result := connect()
+	log.Info(result)
 }
 
-// ハンドラ
-func bookHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, getBook())
-}
-
-func getBook() *Book {
-	b := &Book{
-		Title:  "Golangの本",
-		Author: "Golangの本の著者",
+func connect() {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open("postgres", databaseUrl)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	} else {
+		return db
 	}
-	return b
 }
