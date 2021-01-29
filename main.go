d51b7a4d8950e7f35848f389ed5795718800021e
福田皓一 <k.fukuda@shift7.jp>
2021-01-29 17:55:10 +0900

    push

--- main.go
+++ main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"log"
+	"github.com/labstack/echo"
+	"net/http"
 	"os"
 )
 
+type Connect struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 func main() {
+	// Echo インスタンスをNew
+	e := echo.New()
+
+	// ルーティング設定
+	e.GET("/connects/:id", connectHandler)
+
+	// サーバ起動
+	e.Start(":" + os.Getenv("PORT"))
+}
+
+// ハンドラ
+func connectHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, getConnect())
+}
+
+func getConnect() *Book {
 	databaseUrl := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open("postgres", databaseUrl)
 	log.Print(db)
