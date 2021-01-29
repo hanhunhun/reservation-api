065ef9885ef9d8155522c1d969aecedf3da2b61c
福田皓一 <k.fukuda@shift7.jp>
2021-01-29 15:33:48 +0900

    push

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"os"
+)
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	// Echo インスタンスをNew
+	e := echo.New()
+
+	// ルーティング設定
+	e.GET("/books/:id", bookHandler)
+
+	// サーバ起動
+	e.Start(":" + os.Getenv("PORT"))
+}
+
+// ハンドラ
+func bookHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, getBook())
+}
+
+func getBook() *Book {
+	b := &Book{
+		Title:  "Golangの本",
+		Author: "Golangの本の著者",
+	}
+	return b
 }
